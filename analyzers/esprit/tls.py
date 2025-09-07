a782fc575fb7bacd7e84342fe38de4b3dfa39d81
Akira Tamamori <tamamori5917@gmail.com>
2025-09-07 15:40:47 +0900

    Update tls.py

--- analyzers/esprit/tls.py
+++ analyzers/esprit/tls.py
@@ -110,9 +110,9 @@ class TLSEspritAnalyzer(EspritAnalyzerBase):
 
         # Extract and sort only pairs with positive frequencies
         positive_freq_indices = np.where(estimated_freqs_hz > 0)[0]
-
         sorted_indices = np.argsort(estimated_freqs_hz[positive_freq_indices])
         freqs = estimated_freqs_hz[positive_freq_indices][sorted_indices]
+
         return freqs.astype(np.float64)
 
 
