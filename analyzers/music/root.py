0eb2df9b048dfd485799b06e2be1997486363452
Akira Tamamori <tamamori5917@gmail.com>
2025-09-12 15:18:49 +0900

    add subspace ratio

--- analyzers/music/root.py
+++ analyzers/music/root.py
@@ -37,7 +37,13 @@ from .base import MusicAnalyzerBase
 class RootMusicAnalyzer(MusicAnalyzerBase):
     """MUSIC analyzer using polynomial rooting."""
 
-    def __init__(self, fs: float, n_sinusoids: int, sep_factor: float = 0.4):
+    def __init__(
+        self,
+        fs: float,
+        n_sinusoids: int,
+        sep_factor: float = 0.4,
+        subspace_ratio: float = 1 / 3,
+    ):
         """Initialize the analyzer with an experiment configuration.
 
         Args:
@@ -45,8 +51,10 @@ class RootMusicAnalyzer(MusicAnalyzerBase):
             n_sinusoids (int): Number of sinusoids.
             sep_factor (float, optional):
                 Separation factor for resolving close frequencies.
+            subspace_ratio (float, optional): The ratio of the subspace dimension
+                to the signal length. Should be between 0 and 0.5. Defaults to 1/3.
         """
-        super().__init__(fs, n_sinusoids)
+        super().__init__(fs, n_sinusoids, subspace_ratio)
         self.sep_factor: float = sep_factor
 
     @override
