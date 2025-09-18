c2a15ae752f3766b62880ccb831ef2cc2c5a75ee
Akira Tamamori <tamamori5917@gmail.com>
2025-09-19 08:55:30 +0900

    Update _common.py

--- analyzers/_common.py
+++ analyzers/_common.py
@@ -109,7 +109,7 @@ def find_freqs_from_roots(
     """
     # 1. Calculate the roots of a polynomial
     try:
-        roots = poly.polyroots(coefficients[::-1])  # very slow
+        roots = poly.polyroots(coefficients[::-1])
     except np.linalg.LinAlgError:
         warnings.warn("Failed to find roots of the polynomial.")
         return np.array([])
