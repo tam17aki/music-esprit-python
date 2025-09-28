4a841e5f3b972caf5451a83d62010f1c78282d46
Akira Tamamori <tamamori5917@gmail.com>
2025-09-28 21:01:42 +0900

    Update unitary.py

--- analyzers/esprit/unitary.py
+++ analyzers/esprit/unitary.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
-"""Defines UnitaryEspritAnalyzer class to solve frequencies via Unitary ESPRIT.
+"""Defines UnitaryEspritAnalyzer class for Unitary ESPRIT.
 
 Copyright (C) 2025 by Akira TAMAMORI
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import warnings
@@ -36,7 +37,7 @@ from .solvers import LSUnitaryEspritSolver, TLSUnitaryEspritSolver
 
 @final
 class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
-    """A class to solve frequencies via Unitary ESPRIT with least squares."""
+    """A class to solve frequencies via Unitary ESPRIT with LS."""
 
     def __init__(
         self,
@@ -53,8 +54,9 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
             n_sinusoids (int): Number of sinusoids.
             solver (LSUnitaryEspritSolver | TLSUnitaryEspritSolver):
                 Solver to solve frequencies with the rotation operator.
-            subspace_ratio (float, optional): The ratio of the subspace dimension
-                to the signal length. Must be between 0 and 0.5. Defaults to 1/3.
+            subspace_ratio (float, optional): The ratio of the subspace
+                dimension to the signal length. Must be between 0 and
+                0.5. Defaults to 1/3.
         """
         super().__init__(fs, n_sinusoids, subspace_ratio)
         self.solver: LSUnitaryEspritSolver | TLSUnitaryEspritSolver = solver
@@ -80,7 +82,8 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
         # 2. Solve frequencies with the stored solver
         omegas = self.solver.solve(signal_subspace)
 
-        # 3. Post-processes raw angular frequencies to final frequency estimates
+        # 3. Post-processes raw angular frequencies to final frequency
+        #    estimates
         est_freqs = self._postprocess_omegas(omegas)
 
         return est_freqs
@@ -89,7 +92,7 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
     def _estimate_signal_subspace(
         self, signal: npt.NDArray[np.float64] | npt.NDArray[np.complex128]
     ) -> npt.NDArray[np.float64] | None:
-        """Estimate the real-valued signal subspace using the covariance approach.
+        """Estimate the real-valued signal subspace via cov. approach.
 
         This method follows Step 2 of TABLE I in Haardt & Nossek (1995),
         which involves a real-valued SVD of a transformed data matrix.
@@ -98,7 +101,8 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
             signal (np.ndarray): Input signal (float64 or complex128).
 
         Returns:
-            np.ndarray: The real-valued signal subspace matrix (float64).
+            np.ndarray:
+                The real-valued signal subspace matrix (float64).
                 Returns None if estimation fails.
         """
         # 1. Construct the data matrix X (Hankel matrix)
@@ -106,15 +110,16 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
         _data_matrix = self._build_hankel_matrix(signal, self.subspace_dim)
         data_matrix = _data_matrix.astype(np.complex128)
 
-        # 2. Convert complex matrix X to real matrix T(X) (based on Eq. (7))
-        #    The size of T(X) is (L, 2*N)
+        # 2. Convert complex matrix X to real matrix T(X)
+        #    (based on Eq. (7)). The size of T(X) is (L, 2*N)
         try:
             transformed_matrix = self._transform_complex_to_real(data_matrix)
         except ValueError:
             warnings.warn("Failed to transform complex data matrix to real matrix.")
             return None
 
-        # 3. Perform eigenvalue decomposion the Hermetial matrix in Eq. (29)
+        # 3. Perform eigenvalue decomposion the Hermetial matrix in
+        #    Eq. (29)
         cov_matrix = transformed_matrix @ transformed_matrix.conj().T
         try:
             _, eigenvectors = eigh(cov_matrix)
@@ -131,7 +136,7 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
     def _transform_complex_to_real(
         g_matrix: npt.NDArray[np.complex128],
     ) -> npt.NDArray[np.float64]:
-        """Transform a complex matrix G to a real matrix T(G) based on Eq. (7).
+        """Transform a complex matrix G to a real matrix T(G).
 
         Args:
             g_matrix (np.ndarray): Complex matrix G (complex128).
@@ -168,13 +173,15 @@ class UnitaryEspritAnalyzer(EVDBasedEspritAnalyzer):
 
     @override
     def get_params(self) -> AnalyzerParameters:
-        """Return the analyzer's hyperparameters, including spectral-specific ones.
+        """Return the analyzer's hyperparameters.
 
-        Extends the base implementation to include the name of the solver class.
+        Extends the base implementation to include the name of the
+        solver class and the hyperparameter specific to the
+        Unitary ESPRIT method.
 
         Returns:
             AnalyzerParameters:
-                A TypedDict containing both common and spectral-specific
+                A TypedDict containing both common and method-specific
                 hyperparameters.
         """
         params = super().get_params()
