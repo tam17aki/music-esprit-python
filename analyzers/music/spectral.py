622f10c06de277e886dabff4ee3bf160e52bac91
Akira Tamamori <tamamori5917@gmail.com>
2025-09-30 23:28:57 +0900

    Update spectral.py

--- analyzers/music/spectral.py
+++ analyzers/music/spectral.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Defines SpectralMusicAnalyzer class for Spectral MUSIC.
+"""Defines SpectralMusicAnalyzer class for Spectral MUSIC method.
 
 Copyright (C) 2025 by Akira TAMAMORI
 
