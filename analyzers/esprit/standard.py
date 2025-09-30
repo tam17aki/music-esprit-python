b851f1060068ae393c7f0b604ba9ce0fcab3a60c
Akira Tamamori <tamamori5917@gmail.com>
2025-09-30 23:39:29 +0900

    Update standard.py

--- analyzers/esprit/standard.py
+++ analyzers/esprit/standard.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Defines StandardEspritAnalyzer class for ESPRIT method.
+"""Defines StandardEspritAnalyzer class for the ESPRIT method.
 
 Copyright (C) 2025 by Akira TAMAMORI
 
