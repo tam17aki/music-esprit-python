2e6faf045f89c5332827e3684287b70a75703fbb
Akira Tamamori <tamamori5917@gmail.com>
2025-10-07 12:53:01 +0900

    Update fft.py

--- analyzers/esprit/fft.py
+++ analyzers/esprit/fft.py
@@ -39,7 +39,11 @@ from utils.data_models import (
 from .._common import estimate_freqs_iterative_fft
 from ..models import AnalyzerParameters
 from .base import EspritAnalyzerBase
-from .solvers import FastEspritSolverType, fast_esprit_solvers
+from .solvers import (
+    FastEspritSolveFunction,
+    FastEspritSolverType,
+    fast_esprit_solvers,
+)
 
 
 @final
@@ -73,6 +77,8 @@ class FFTEspritAnalyzer(EspritAnalyzerBase):
         Estimates for Line Spectral Estimation," PREPRINT,
         2023. (Specifically, Algorithm 4)
     """
+
+    solver: FastEspritSolveFunction
 
     def __init__(
         self,
